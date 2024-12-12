3647e75da07ee15c53e8a85ced21914969dc0716
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-12-12 10:00:43 +0330

    feat: config react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import Layout from "./layout/Layout";
+import Router from "./router/Router";
+import defaultOptions from "./configs/reactQuery";
 
 function App() {
+  const queryClient = new QueryClient({ defaultOptions });
+
   return (
-    <BrowserRouter>
-      
-    </BrowserRouter>
-  )
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Layout>
+          <Router />
+        </Layout>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
