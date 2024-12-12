3e06373140e10b3b2eab0bfd156e73f1c797fa07
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-12-12 20:01:28 +0330

    refavtor: SimpleWorksBoxSection.jsx and CompaniesSection.jsx files

--- src/components/templates/CompaniesSection.jsx
+++ src/components/templates/CompaniesSection.jsx
@@ -1,19 +1,33 @@
+import { useEffect } from "react";
 import { FaFacebookF } from "react-icons/fa6";
 import { SiTinder } from "react-icons/si";
 import { SiAirbnb } from "react-icons/si";
 import { SiHubspot } from "react-icons/si";
 import { GrAmazon } from "react-icons/gr";
+import AOS from "aos";
 
 import SharedTopSection from "../shared/SharedTopSection";
 import { Link } from "react-router-dom";
 
+import "aos/dist/aos.css";
+
 function CompaniesSection() {
+  useEffect(() => {
+    AOS.init();
+  });
+
   return (
     <div className="px-12">
-      <SharedTopSection
-        title="Trusted by over 20,000 companies all over the world"
-        desc="Arcu cursus vitae congue mauris rhoncus viverra nibh cras pulvinar mattis blandit libero cursus mattis."
-      />
+      <div
+        data-aos="zoom-in"
+        data-aos-offset="200"
+        data-aos-easing="ease-in-sine"
+      >
+        <SharedTopSection
+          title="Trusted by over 20,000 companies all over the world"
+          desc="Arcu cursus vitae congue mauris rhoncus viverra nibh cras pulvinar mattis blandit libero cursus mattis."
+        />
+      </div>
       <div className="flex justify-between child:flex child:flex-col child:items-center child:gap-y-1.5 child:text-3xl">
         <Link to="https://www.facebook.com/">
           <FaFacebookF />
--- src/components/templates/SimpleWorksBoxSection.jsx
+++ src/components/templates/SimpleWorksBoxSection.jsx
@@ -1,14 +1,22 @@
+import { useEffect } from "react";
 import { BiTransferAlt } from "react-icons/bi";
 import { TbBrandFlightradar24 } from "react-icons/tb";
 import { IoContractSharp } from "react-icons/io5";
 import { ImExit } from "react-icons/im";
 import { TfiLayers } from "react-icons/tfi";
 import { TbStarsFilled } from "react-icons/tb";
+import AOS from "aos";
 
 import { simpleWorksBoxesInfos } from "../../constants/data";
 
+import "aos/dist/aos.css";
+
 function SimpleWorksBoxSection() {
   const simpleWorksBoxsInfosArray = simpleWorksBoxesInfos;
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
 
   return (
     <div className="p-12 bg-zinc-800">
@@ -16,6 +24,9 @@ function SimpleWorksBoxSection() {
         {simpleWorksBoxsInfosArray.map((simpleWorksBoxesInfo) => (
           <div
             key={simpleWorksBoxesInfo.id}
+            data-aos="flip-right"
+            data-aos-offset="200"
+            data-aos-easing="ease-in-sine"
             className="flex flex-col items-center justify-center gap-y-2 bg-zinc-700 rounded-md h-[150px]"
           >
             <div className="bg-violet-500 w-12 h-12 rounded-full flex items-center justify-center text-3xl">
