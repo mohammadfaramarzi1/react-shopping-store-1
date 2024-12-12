e4ed38ddd86269083784918f041c31daf4267734
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-12-12 09:52:08 +0330

    feat: handle routes

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "../pages/HomePage";
+import AboutUsPage from "../pages/AboutUsPage";
+import CheckOutPage from "../pages/CheckOutPage";
+import NotFoundPage from "../pages/NotFoundPage";
+import ProductDetailPage from "../pages/ProductDetailPage";
+import ProductsPage from "../pages/ProductsPage";
 
 function Router() {
   return (
     <Routes>
-        <Route index element={<HomePage />} />
+      <Route index element={<HomePage />} />
+      <Route path="/about" element={<AboutUsPage />} />
+      <Route path="/checkout" element={<CheckOutPage />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/products/:id" element={<ProductDetailPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
-  )
+  );
 }
 
-export default Router
+export default Router;
