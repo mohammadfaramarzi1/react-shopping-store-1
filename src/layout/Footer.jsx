b9bd26767734488459783b85a232548f1691b69d
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-12-12 11:32:21 +0330

    feat: template Footer.jsx

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { FaGithubSquare } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="flex justify-between items-center">
+      <p className="py-4 text-xl">
+        Made With <span className="text-violet-500">💜</span>By{" "}
+        <span className="text-violet-500">Mohammad Faramarzi</span>
+      </p>
+      <Link to="https://github.com/mohammadfaramarzi1/" className="text-3xl">
+        <FaGithubSquare />
+      </Link>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
