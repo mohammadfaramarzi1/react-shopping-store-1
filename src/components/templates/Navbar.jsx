df368642db48a0da9b25852e840d3cfa291e4df9
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-12-12 11:24:56 +0330

    feat:complete  template navabr

--- src/components/templates/Navbar.jsx
+++ src/components/templates/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
       <Link to="/" className="font-DosisBold text-5xl text-violet-500 hover:text-violet-700 transition-colors delay-75">
         <h1>Shopping Store</h1>
       </Link>
-      <ul className="flex justify-between gap-4 text-2xl child-hover:text-violet-400 child:transition-colors child:delay-75">
+      <ul className="flex justify-center gap-4 w-80 py-2 px-4 border-2 border-zinc-800 rounded-full text-2xl child-hover:text-violet-400 child:transition-colors child:delay-75">
         <li>
           <Link to="/">Home</Link>
         </li>
