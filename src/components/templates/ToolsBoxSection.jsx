6537c40d02291feb21d2c63c451b7a61503c29ab
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-12-12 18:26:20 +0330

    feat: template ToolsSection.jsx and ToolsBoxSection.jsx file

--- src/components/templates/ToolsBoxSection.jsx
+++ src/components/templates/ToolsBoxSection.jsx
@@ -2,8 +2,19 @@ import { SlEnergy } from "react-icons/sl";
 import { BsFillSendFill } from "react-icons/bs";
 import { AiFillMoon } from "react-icons/ai";
 
-function ToolsBoxSection() {
-  return <div>ToolsBoxSection</div>;
+function ToolsBoxSection({ title, desc, icon, id }) {
+  const iconName = icon;
+  return (
+    <div className={id === 2 ? "bg-zinc-900 mb-4 rounded-md p-2 border-4 border-violet-500" : "bg-zinc-800 mb-4 rounded-md p-2 border-4 border-zinc-600"}>
+      <h2 className="text-2xl mb-2">{title}</h2>
+      <div className="flex items-center justify-between">
+        <p className="text-lg opacity-70 max-w-80">{desc}</p>
+        {iconName === "SlEnergy" && <SlEnergy />}
+        {iconName === "BsFillSendFill" && <BsFillSendFill />}
+        {iconName === "AiFillMoon" && <AiFillMoon />}
+      </div>
+    </div>
+  );
 }
 
 export default ToolsBoxSection;
--- src/components/templates/ToolsSection.jsx
+++ src/components/templates/ToolsSection.jsx
@@ -1,12 +1,24 @@
+import { useEffect } from "react";
 import { toolsItems } from "../../constants/data";
 import ToolsBoxSection from "./ToolsBoxSection";
+import AOS from "aos";
+
+import "aos/dist/aos.css";
 
 function ToolsSection() {
   const toolsItemsArray = toolsItems;
-  console.log(toolsItemsArray);
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
-    <div>
-      <div>
+    <div className="flex gap-10">
+      <div
+        className="-mb-10 min-w-[600px]"
+        data-aos="fade-right"
+        data-aos-offset="200"
+        data-aos-easing="ease-in-sine"
+      >
         <div className="pb-4">
           <h3 className="text-xl mb-3">Powerful suite of tools</h3>
           <p className="opacity-70 max-w-md text-lg">
@@ -20,6 +32,13 @@ function ToolsSection() {
             <ToolsBoxSection key={toolsItem.id} {...toolsItem} />
           ))}
         </div>
+      </div>
+      <div className="w-[600px] flex items-center justify-center">
+        <img
+          src="/illustration1-mvQYH-I0.svg"
+          alt="cover"
+          className="block"
+        />
       </div>
     </div>
   );
