f4949f34b7168626e68798a953b2445b59855479
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-12-13 11:26:43 +0330

    feat: ProductsBox.jsx template

--- src/components/templates/ProductsBox.jsx
+++ src/components/templates/ProductsBox.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
 
-function ProductsBox() {
+function ProductsBox({ products }) {
+  const mainProducts = products.data.filter(
+    (product) => product.title !== "New Product"
+  );
+
   return (
-    <div>
-        
-    </div>
-  )
+    <>
+      {mainProducts.map((product) => (
+        <div key={product.id} className="w-80 border-2 border-zinc-800 p-6 rounded-md">
+          <img src={product.images[0]} alt={product.title} className="rounded-md mb-5" />
+          <h2 className="text-xl font-bold">{product.title}</h2>
+          <p className="opacity-70 line-clamp-2 my-2 text-lg">{product.description}</p>
+          <span className="inline-block bg-zinc-800 p-2 rounded-full my-2">{product.category.name}</span>
+          <div className="flex justify-between mt-2">
+            <div className="flex flex-col">
+              <span className="opacity-70">Price</span>
+              <span className="font-bold text-lg">${product.price}</span>
+            </div>
+            <button className="bg-violet-500 w-32 h-10 rounded-md hover:bg-violet-400 transition-colors delay-75">Add To Cart</button>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 }
 
-export default ProductsBox
+export default ProductsBox;
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
-import SearchBox from '../components/templates/SearchBox'
-import ProductsBox from '../components/templates/ProductsBox'
+import { useQuery } from "@tanstack/react-query";
+
+import SearchBox from "../components/templates/SearchBox";
+import ProductsBox from "../components/templates/ProductsBox";
+import { getAllProducts } from "../services/products";
 
 function ProductsPage() {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["all-products"],
+    queryFn: () => getAllProducts,
+  });
+
+
   return (
-    <div className='p-12'>
+    <div className="p-12">
       <div>
         <SearchBox />
       </div>
       <div>
-        <div className='grid grid-cols-3 gap-3'>
-          <ProductsBox />
+        <div className="grid grid-cols-3 mt-10">
+          <ProductsBox products={data} />
         </div>
         <div></div>
       </div>
     </div>
-  )
+  );
 }
 
-export default ProductsPage
+export default ProductsPage;
