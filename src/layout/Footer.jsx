0fe93537038614dba7bca6c12edad5ba917f175c
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2025-02-06 19:24:17 +0330

    feat: responsive Footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <div className="flex justify-between items-center px-12 border-t pt-2 mt-12">
-      <p className="py-4 text-xl">
+      <p className="py-4 text-md sm:text-xl">
         Made With <span className="text-violet-500">💜</span>By{" "}
         <span className="text-violet-500">Mohammad Faramarzi</span>
       </p>
